044002c95991942634a428cb3437f486e4e66a04
fishyboi@MSI <fishyboi@MSI>
2023-09-11 18:31:31 -0400

    slowly, but surely

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await axios.get("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
+      const data = response.data
+      setUsers(data)
+    }
+    getUsers()
+  }, [])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await axios.get("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/posts")
+      const data = response.data
+      setPosts(data)
+    }
+    getPosts()
+  }, [])
 
   return (
     <>
-      <h1>Hello User</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/posts">Posts</Link>
+      </nav>
     </>
   )
 }
