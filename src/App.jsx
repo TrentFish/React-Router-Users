5592b5047a725a903d3728e0dfe37a02d436c7b8
fishyboi@MSI <fishyboi@MSI>
2023-09-11 21:57:00 -0400

    things are working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link, useLocation, Routes, Route } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+const Users = ({ users }) => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {
+          users.map( user => {
+            return (
+              <li>
+                {user.name}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+const Posts = ({ posts }) => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {
+          posts.map( post => {
+            return (
+              <li>
+                {post.title}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   const [users, setUsers] = useState([])
   const [posts, setPosts] = useState([])
+  const location = useLocation()
+  const {pathname} = location
 
   useEffect(() => {
     const getUsers = async () => {
@@ -26,10 +73,15 @@ function App() {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
+        <Link to="/" className={ pathname === "/" ? "selected": ""}>Home</Link>
+        <Link to="/users" className={ pathname === "/users" ? "selected": ""}>Users ({users.length})</Link>
+        <Link to="/posts" className={ pathname === "/posts" ? "selected": ""}>Posts ({posts.length})</Link>
       </nav>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/users" element={ <Users users={ users }/> } />
+        <Route path="/posts" element={ <Posts posts={ posts }/> } />
+      </Routes>
     </>
   )
 }
